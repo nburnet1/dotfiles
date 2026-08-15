e9c129e0ba2cc21a933c92af2a48cb7484b1b8d3
nburnet1 <noah.burnette@intellicintegration.com>
2026-08-14 23:11:03 -0400

    fixed width on front app

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -2,9 +2,21 @@
 
 # Front App Plugin
 # Shows the currently focused application with its native icon
+# Hides icon for apps without proper macOS icons
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set $NAME \
-    label="$INFO" \
-    background.image="app.$INFO"
+  # Apps that don't have proper macOS icons - hide the image
+  case "$INFO" in
+    "loginwindow"|""|"'localhost'"|"localhost")
+      sketchybar --set $NAME \
+        label="$INFO" \
+        background.image.drawing=off
+      ;;
+    *)
+      sketchybar --set $NAME \
+        label="$INFO" \
+        background.image="app.$INFO" \
+        background.image.drawing=on
+      ;;
+  esac
 fi
